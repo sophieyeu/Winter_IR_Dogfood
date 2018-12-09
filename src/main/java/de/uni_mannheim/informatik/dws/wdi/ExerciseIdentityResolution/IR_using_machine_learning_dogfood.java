8b0ad2d2e6e74f1c56d0cc34d85dfcb546cca44e
juriigen <jurgen_ame@hotmail.com>
2018-12-09 19:01:17 +0100

    Jurgen Replace skip gram with cbow.

--- src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/IR_using_machine_learning_dogfood.java
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/IR_using_machine_learning_dogfood.java
@@ -27,8 +27,22 @@ import weka.core.Instances;
 import weka.core.Utils;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import weka.classifiers.Evaluation;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.Utils;
 
 public class IR_using_machine_learning_dogfood {
 
@@ -200,17 +214,19 @@ public class IR_using_machine_learning_dogfood {
 		System.out.println(String.format(
 				"F1: %.4f", perfTest.getF1()));
 
-		System.out.println("Embeddings started");
-		JFastText jft = new JFastText();
-		jft.runCmd(new String[] {
-				"skipgram",
-				"-input", "data/goldstandard/test_dogfood_final_300.csv",
-				"-output", "data/output/dogfood_embeddings.csv",
-				"-bucket", "100",
-				"-minCount", "1"
-		});
+        System.out.println("Embeddings started");
+        JFastText jft = new JFastText();
+        jft.runCmd(new String[]{
+                "cbow",
+                "-input", "data/input/dogfood_Title.csv",
+                "-output", "data/output/dogfood_Title_embeddings_cbow.model",
+                "-bucket", "100",
+                "-minCount", "1"
+        });
+
 
         //Deep learning neural network
+        Date beginDate = new Date();
         //network variables
         String backPropOptions =
                 "-L " + 0.1 //learning rate
@@ -219,8 +235,7 @@ public class IR_using_machine_learning_dogfood {
                         + " -V " + 0 //validation
                         + " -S " + 0 //seed
                         + " -E " + 0 //error
-                        + " -H " + "3";
-        // hidden nodes. //e.g. use "3,3" for 2 level hidden layer with 3 nodes
+                        + " -H " + "3"; // hidden nodes. //e.g. use "3,3" for 2 level hidden layer with 3 nodes
 
         try {
             //prepare historical data
@@ -233,42 +248,112 @@ public class IR_using_machine_learning_dogfood {
             trainingset.setClassIndex(trainingset.numAttributes() - 1);
             //final attribute in a line stands for output
 
-            //network training
+
+//            //network training
             MultilayerPerceptron mlp = new MultilayerPerceptron();
+            //MultilayerPerceptron mlp = readFromFile("C:\\Users\\User\\workspace\\Winter_IR_Dogfood\\data\\output\\");
+//			System.out.println("network weights and structure are load...");
             mlp.setOptions(Utils.splitOptions(backPropOptions));
             mlp.buildClassifier(trainingset);
             System.out.println("final weights:");
             System.out.println(mlp);
-
+            byte[] binaryNetwork = serialize(mlp);
+            writeToFile(binaryNetwork,  "C:\\Users\\User\\workspace\\Winter_IR_Dogfood\\data\\output\\");
             //display actual and forecast values
             System.out.println("\nactual\tprediction");
-            for (int i = 0; i <= trainingset.numInstances(); i++) {
+            for (int i = 0; i < trainingset.numInstances(); i++) {
 
                 double actual = trainingset.instance(i).classValue();
                 double prediction =
                         mlp.distributionForInstance(trainingset.instance(i))[0];
 
-                System.out.println(actual+"\t"+prediction);
+                System.out.println(actual + "\t" + prediction);
 
             }
 
             //success metrics
-            System.out.println( "\nSuccess Metrics: ");
+            System.out.println("\nSuccess Metrics: ");
             Evaluation eval = new Evaluation(trainingset);
             eval.evaluateModel(mlp, trainingset);
 
+
             //display metrics
-            System.out.println("Correlation: "+eval.correlationCoefficient());
-            System.out.println("MAE: "+eval.meanAbsoluteError());
-            System.out.println("RMSE: "+eval.rootMeanSquaredError());
-            System.out.println("RAE: "+eval.relativeAbsoluteError()+"%");
-            System.out.println("RRSE: "+eval.rootRelativeSquaredError()+"%");
-            System.out.println("Instances: "+eval.numInstances());
+            //System.out.println("Correlation: " + eval.correlationCoefficient());
+            System.out.println("MAE: " + eval.meanAbsoluteError());
+            System.out.println("RMSE: " + eval.rootMeanSquaredError());
+            System.out.println("RAE: " + eval.relativeAbsoluteError() + "%");
+            System.out.println("RRSE: " + eval.rootRelativeSquaredError() + "%");
+            System.out.println("Instances: " + eval.numInstances());
+            Date endDate = new Date();
+
+         System.out.println("\nprogram ends in " +(double)(endDate.getTime() - beginDate.getTime())/1000+" seconds\n");
 
         } catch (Exception ex) {
 
             System.out.println(ex);
 
         }
+    }
+
+
+    public static byte[] serialize(Object obj) throws Exception {
+
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        ObjectOutputStream o = new ObjectOutputStream(b);
+        o.writeObject(obj);
+        return b.toByteArray();
+
+    }
+
+    public static Object deserialize(byte[] bytes) throws Exception {
+
+        ByteArrayInputStream b = new ByteArrayInputStream(bytes);
+
+        ObjectInputStream o = new ObjectInputStream(b);
+
+        return o.readObject();
+
+    }
+
+    public static void writeToFile(byte[] binaryNetwork, String dumpLocation) throws Exception {
+
+        FileOutputStream stream = new FileOutputStream(dumpLocation + "trained_network.txt");
+        stream.write(binaryNetwork);
+        stream.close();
+
+    }
+
+    public static MultilayerPerceptron readFromFile(String dumpLocation) {
+
+        MultilayerPerceptron mlp = new MultilayerPerceptron();
+
+        //binary network is saved to following file
+        File file = new File(dumpLocation + "trained_network.txt");
+
+        FileInputStream fileInputStream = null;
+
+        //binary content will be stored in the binaryFile variable
+        byte[] binaryFile = new byte[(int) file.length()];
+
+        try {
+
+            fileInputStream = new FileInputStream(file);
+            fileInputStream.read(binaryFile);
+            fileInputStream.close();
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        try {
+
+            mlp = (MultilayerPerceptron) deserialize(binaryFile);
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+
+        return mlp;
+
     }
 }
